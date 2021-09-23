22a526f1965409cdafe315097bffbeea91e73850
Nick Painter <npainter@danfoss.com>
2021-09-23 15:37:33 -0500

    Solution added to Github

---
+++ OMIcheck.ps1
@@ -0,0 +1,137 @@
+﻿# This script lists the virtual machines in Azure subscriptions of the user,
+# and looks for vulnerable OMI packages. The report is grouped by subscription and resource group name.
+
+# Set below flag to true if you want vulnerable OMI to be patched.
+# If OMSLinuxAgent is installed, then a minor update will be triggered.
+# If OMI is standalone, the latest bits will be installed from Github.
+
+param(
+    [Parameter(HelpMessage = "Perform OMI version upgrade.  If this is not set, the tool will display Linux VMs and show vulnerability status.")]
+    [switch]$upgradeOMI = $false
+)
+
+Write-Host "UpgradeOMI was set to "$upgradeOMI"."
+
+if ($upgradeOMI -eq $false) {
+    Write-Host "Printing vulnerability status for all VMs that the user has access to."
+    Write-Warning "Run this script with the -upgradeOMI flag in order to actually patch vulnerable machines."
+}
+
+if ($upgradeOMI -eq $true) {
+    Write-Host "The OMI agent will be updated on all vulnerable Azure Linux VMs that the user running the script has access to."
+}
+# $upgradeOMI = $false; #detect only
+$omsVersionGood = "1.13"
+
+# Update these paths accordingly. In cloud shell, the path is usually /home/user_name
+
+## ORIGINALS
+# $checkScriptPath  = "C:\linux\OMIcheck\omi_check.sh"
+# $upgradeScriptPath = "C:\linux\OMIcheck\omi_upgrade.sh"
+
+## NEW
+$checkScriptPath = "C:\projects\OMS-Agent-for-Linux\tools\OMIcheck\omi_check.sh"
+$upgradeScriptPath = "C:\projects\OMS-Agent-for-Linux\tools\OMIcheck\omi_upgrade.sh"
+
+# Get all Azure Subscriptions
+Connect-AzAccount #-UseDeviceAuthentication
+$subs = Get-AzSubscription
+
+# For each subscription execute to find (and upgrade) OMI on Linux VMs
+# TODO: This can be converted to ForEach-Object -Parallel. However, it requires PSh >= 7.1
+foreach ($sub in $subs) {
+    #DEBUG: Limit to one subscription only.
+    #if ($sub.Name -eq "My subscription name here")
+    #{
+    # Set Azure Subscription context    
+    Set-AzContext -Subscription $sub.Id
+
+    Write-Output "Listing Virtual Machines in subscription '$($sub.Name)'"
+    $VMs = Get-AzVM -Status
+    $VMsSorted = $VMs | Sort-Object -Property ResourceGroupName
+    $PreviousRG = ""
+    foreach ($VM in $VMs) {
+        #DEBUG: limit to 1 vm only
+        #if ($vm.name -ne "my-testing-vm")
+        #{
+        #    continue
+        #}
+
+        # Group VMs by resource groups.
+        if ($PreviousRG -ne $VM.ResourceGroupName) {
+            Write-Host "`tResourceGroup: " $VM.ResourceGroupName
+            $PreviousRG = $VM.ResourceGroupName
+        }
+
+        # Update only running VMs.
+        if ($VM.PowerState -ne "VM running") {
+            Write-Host -ForegroundColor Gray `t`t $VM.Name " in resource group : VM is not running"
+            continue
+        }
+
+        # Update Linux only VMs.
+        if ($VM.StorageProfile.OsDisk.OsType.ToString() -ne "Linux") {
+            Write-Host -ForegroundColor Gray `t`t $VM.Name ": VM is not running Linux OS"
+            continue
+        }
+
+        # TODO: Consider setting timeout. Parameter does not exist, -AsJob is an option for v2.
+        $check = Invoke-AzVMRunCommand -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name -CommandId 'RunShellScript' -ScriptPath $checkScriptPath
+
+        $split = $check.Value.Message.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
+        if ($check.Value.Message.Contains("/opt/omi/bin/omiserver:")) {
+            $pkgVer = $split[3]
+            if ($pkgVer -eq "OMI-1.6.8-1") {
+                Write-Host -ForegroundColor Green `t`t $VM.Name ": VM has patched OMI version " $pkgVer
+            }
+            else {
+                Write-Host -ForegroundColor Red `t`t $VM.Name ": VM has vulnerable OMI version " $pkgVer
+                if ($upgradeOMI) {
+                    # If LAD extension is installed, nudge the Azure Guest Agent to pickup the latest bits. 
+                    $VMobj = Get-AzVM -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name
+                    $ladExt = $VMobj.Extensions | where { ($_.Publisher -eq "Microsoft.Azure.Diagnostics" -and $_.VirtualMachineExtensionType -eq "LinuxDiagnostic") }
+                    if ($ladExt -ne $null) {
+                        if ($ladExt.ProvisioningState -eq "Succeeded") {
+                            # Trigger a goal state change for GA.
+                            Set-AzVMExtension -VMName $VM.Name -ResourceGroupName $VM.ResourceGroupName -Location $VM.Location -Publisher $ladExt.Publisher -Name $ladExt.Name -ExtensionType $ladExt.VirtualMachineExtensionType -TypeHandlerVersion $ladExt.TypeHandlerVersion
+                            Write-Host -ForegroundColor Green `t`t $VM.Name ": Set LinuxDiagnostic extension goal state for update"
+                            continue;
+                        }
+                        else {
+                            Write-Host -ForegroundColor Gray `t`t $VM.Name ": LinuxDiagnostic extension is not in succeeded state. Skipping."
+                        }
+                    }
+
+                    # If OMSLinuxAgent is installed, nudge the Azure Guest Agent to pickup the latest bits.
+                    $omsExt = $VMobj.Extensions | where { ($_.Publisher -eq "Microsoft.EnterpriseCloud.Monitoring" -and $_.VirtualMachineExtensionType -eq "OmsAgentForLinux") }
+                    if ($omsExt -ne $null) {
+                        if ($omsExt.ProvisioningState -eq "Succeeded") {
+                            # Trigger a goal state change for GA.
+                            Set-AzVMExtension -VMName $VM.Name -ResourceGroupName $VM.ResourceGroupName -Location $VM.Location -Publisher $omsExt.Publisher -Name $omsExt.Name -ExtensionType $omsExt.VirtualMachineExtensionType -TypeHandlerVersion $omsExt.TypeHandlerVersion
+                            Write-Host -ForegroundColor Green `t`t $VM.Name ": Set OMSLinuxAgent extension goal state for update"
+                            continue
+                        }
+                        else {
+                            Write-Host -ForegroundColor Gray `t`t $VM.Name ": OMSLinuxAgent extension is not in succeeded state. Skipping."
+                        }
+                    }
+
+                    # Upgrade standalone OMI if none of the above succeeds.
+                    $upgrade = Invoke-AzVMRunCommand -ResourceGroupName $VM.ResourceGroupName -Name $VM.Name -CommandId 'RunShellScript' -ScriptPath $upgradeScriptPath
+                    Write-Host -ForegroundColor Red `t`t $VM.Name ": Result of OMI package upgrade attempt: " $upgrade.Value.Message
+                }
+            }
+        }
+        else {
+            Write-Host -ForegroundColor Gray `t`t $VM.Name ": VM has no OMI package"
+        }
+
+        #DEBUG:
+        #break
+    }
+    #}
+}
+
+if ($upgradeOMI -eq $false) {
+    Write-Warning "Reminder: Run this script with the -upgradeOMI flag in order to actually patch vulnerable machines!"
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# Purpose
+
+This tool detects vulnerable OMI installations (< 1.6.8.1) in your subscriptions.  By passing the `upgradeOMI` flag it can also upgrade the vulnerable OMI agents.
+
+https://msrc-blog.microsoft.com/2021/09/16/additional-guidance-regarding-omi-vulnerabilities-within-azure-vm-management-extensions/
+
+https://arstechnica.com/information-technology/2021/09/security-researchers-at-wiz-discover-another-major-azure-vulnerability/
+
+https://www.wiz.io/blog/secret-agent-exposes-azure-customers-to-unauthorized-code-execution
+
+# Using the Script
+
+1. Review the script and update the paths accordingly.  For example, if the project was cloned into `C:\projects` the paths contained within the script would be.
+```
+$checkScriptPath = "C:\projects\OMS-Agent-for-Linux\tools\OMIcheck\omi_check.sh"
+$upgradeScriptPath = "C:\projects\OMS-Agent-for-Linux\tools\OMIcheck\omi_upgrade.sh"
+```
+2. You can run this script from cloud shell in Azure portal or Windows/Linux/MacOS desktop, make sure to update Az.Compute modules to the latest and or your Powershell version.
+
+```
+Install-Module -Name Az.Compute
+Update-Module -Confirm Az.Compute
+Get-Module -Name Az.Compute
+```
+
+3. The tool uses the credentials of the user to list the subscriptions and VMs.  To display the vulnerability status of all Azure Linux VMs that the user has access to run the following command from the directory containing `OMIcheck.ps1`.
+
+```
+.\OMIcheck.ps1
+```
+
+4. To update all of the vulnerable VMs and patch the vulnerability, run the command with the `-upgradeOMI` flag.
+```
+.\OMIcheck.ps1 -upgradeOMI
+```
+
+# Additional Considerations:
+- You can run the script back to back more than once for validations.
+- The tool can only detect vulnerable VMs that the user running the script has access to.
+- The tool can take quite a long time to run.  You may receive errors if you attempt to run multiple instances of the script or if you try to cancel it and then run it again in a short period of time.
